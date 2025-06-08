153a8ae697de2ba7e0a08b618a238cf64ac8a436
Ki-fi <k.meekels@gmail.com>
2025-06-08 11:12:10 +0200

    State aan toggle button toegevoegd

--- src/components/toggle-button/ToggleButton.jsx
+++ src/components/toggle-button/ToggleButton.jsx
@@ -1,10 +1,24 @@
 import './ToggleButton.css';
+import React from "react";
 
-function ToggleButton({ buttonNameLeft, buttonNameRight, onClick }) {
+function ToggleButton({ buttonNameLeft, buttonNameRight }) {
+
+    const [selected, setSelected] = React.useState('left');
+
+    const handleToggle = (side) => {
+        setSelected(side);
+    }
+
     return (
         <div className="button-wrapper">
-            <button className="button toggle-button left" onClick={onClick}>{buttonNameLeft}</button>
-            <button className="button toggle-button right" onClick={onClick}>{buttonNameRight}</button>
+            <button
+                className={`button toggle-button left ${selected === 'left' ? 'active' : ''}`}
+                onClick={() => handleToggle("left")}>{buttonNameLeft}
+            </button>
+            <button
+                className={`button toggle-button right ${selected === 'right' ? 'active' : ''}`}
+                onClick={() => handleToggle("right")}>{buttonNameRight}
+            </button>
         </div>
     )
 }
